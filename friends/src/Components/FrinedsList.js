2a9efadcacd13ef308ce650bc1544229f04da7ac
Mike Harley <mharl@Mike.harley-enterprises.me>
2019-06-19 15:50:35 -0700

    WIP

---
+++ friends/src/Components/FrinedsList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class FriendsList extends Component {
+  render() {
+   
+   
+  
+    return (
+    <div>
+
+    <Form>
+       <h3> My Friends Form'</h3>
+       <imput placeholder='name'/>
+       <imput placeholder='e-mail'/>
+        <imput placeholder='phone number'/>
+        <button id ='frndbtn'></button>
+        </Form>
+    </div>
+    )
+  };
+}
