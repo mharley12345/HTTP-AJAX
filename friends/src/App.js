554de826b2a6c855d1aea5a2304af7e2587b5895
Mike Harley <mharley12345@gmail.com>
2019-06-21 11:51:57 -0700

    Added some styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import FrindsList from '../src/Components/'friendslist'
+import React, { Component } from "react";
+import "./App.css";
+import ListOfFriends from "./Components/ListOfFriends";
+import FriendForm from "./Components/FriendForm";
+import axios from "axios";
 
-function App() 
-  return (
-    
-
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+  newFriend = data => {
+    this.setState({ friends: data });
+  };
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(response => {
+        console.log(response);
+        const friends = response.data;
+        this.setState({ friends });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+       
+          <h1 className="App-title">My Friends List</h1>
+        </header>
+        <ListOfFriends friends={this.state.friends} />
+        <FriendForm newFriend={this.newFriend} />
+      </div>
+    );
+  }
 }
 
 export default App;
